8f6fca0b7760e7da339ef5e8dbe52eb3da26db20
dangowans <d.gowans@cityssm.on.ca>
2022-02-10 15:28:52 -0500

    test location maintenance

--- cypress/integration/01-admin/location-maint.js
+++ cypress/integration/01-admin/location-maint.js
@@ -1,13 +1,67 @@
 import { testAdmin } from "../../../test/_globals.js";
 import { logout, login } from "../../support/index.js";
+import { randomString } from "../../support/utilities.js";
 describe("Admin - Locations", function () {
     before(function () {
         logout();
         login(testAdmin);
     });
-    after(logout);
     it("Loads page", function () {
         cy.visit("/admin/locations");
         cy.location("pathname").should("equal", "/admin/locations");
+    });
+    it("Adds three new locations", function () {
+        var _loop_1 = function (index) {
+            cy.get("button[data-cy='add-location']").click();
+            cy.get(".modal").should("be.visible");
+            var locationSuffix = "-" + randomString();
+            cy.fixture("location.json").then(function (locationData) {
+                var locationKey = locationData.locationKeyPrefix + locationSuffix;
+                var locationName = locationData.locationNamePrefix + locationSuffix;
+                cy.get(".modal input[name='locationKey']").type(locationKey);
+                cy.get(".modal select[name='locationClassKey']").select(1);
+                cy.get(".modal input[name='locationName']").type(locationName);
+                cy.get(".modal form").submit();
+            });
+            cy.get(".modal").should("not.exist");
+            cy.fixture("location.json").then(function (locationData) {
+                var locationName = locationData.locationNamePrefix + locationSuffix;
+                cy.get("[data-cy='results'] a").contains(locationName);
+            });
+        };
+        for (var index = 0; index < 3; index += 1) {
+            _loop_1(index);
+        }
+    });
+    it("Updates a location", function () {
+        cy.get("[data-cy='results'] a")
+            .first()
+            .click();
+        cy.get(".modal")
+            .should("be.visible")
+            .should("have.length", 1);
+        cy.get(".modal [name='locationClassKey']").select(2);
+        var newLocationName = "New Location-" + randomString();
+        cy.get(".modal [name='locationName']").clear().type(newLocationName);
+        cy.get(".modal form").submit();
+        cy.get(".modal").should("not.exist");
+        cy.get("[data-cy='results'] a")
+            .contains(newLocationName);
+    });
+    it("Removes a location", function () {
+        cy.get("[data-cy='results'] a")
+            .first()
+            .click({ force: true });
+        cy.get(".modal")
+            .should("be.visible")
+            .should("have.length", 1);
+        cy.get(".modal [name='locationName']").invoke("val").then(function (locationName) {
+            cy.get(".modal [data-cy='remove']").click();
+            cy.focused().click();
+            cy.get(".modal").should("not.exist");
+            cy.get("[data-cy='results'] a")
+                .contains(locationName)
+                .should("have.length", 0);
+        });
     });
 });
--- cypress/integration/01-admin/location-maint.ts
+++ cypress/integration/01-admin/location-maint.ts
@@ -3,7 +3,7 @@
 import { testAdmin } from "../../../test/_globals.js";
 
 import { logout, login } from "../../support/index.js";
-// import { randomString } from "../../support/utilities.js";
+import { randomString } from "../../support/utilities.js";
 
 
 describe("Admin - Locations", () => {
@@ -13,10 +13,90 @@ describe("Admin - Locations", () => {
     login(testAdmin)
   });
 
-  after(logout);
+  //after(logout);
 
   it("Loads page", () => {
     cy.visit("/admin/locations");
     cy.location("pathname").should("equal", "/admin/locations");
+  });
+
+  it("Adds three new locations", () => {
+
+    for (let index = 0; index < 3; index += 1) {
+
+      cy.get("button[data-cy='add-location']").click();
+
+      cy.get(".modal").should("be.visible");
+
+      const locationSuffix = "-" + randomString();
+
+      cy.fixture("location.json").then((locationData) => {
+
+        const locationKey = locationData.locationKeyPrefix + locationSuffix;
+        const locationName = locationData.locationNamePrefix + locationSuffix;
+
+        cy.get(".modal input[name='locationKey']").type(locationKey);
+
+        cy.get(".modal select[name='locationClassKey']").select(1);
+        cy.get(".modal input[name='locationName']").type(locationName);
+
+        cy.get(".modal form").submit();
+      });
+
+      cy.get(".modal").should("not.exist");
+
+      cy.fixture("location.json").then((locationData) => {
+        const locationName = locationData.locationNamePrefix + locationSuffix;
+        cy.get("[data-cy='results'] a").contains(locationName);
+      });
+    }
+  });
+
+  it("Updates a location", () => {
+
+    cy.get("[data-cy='results'] a")
+      .first()
+      .click();
+
+    cy.get(".modal")
+      .should("be.visible")
+      .should("have.length", 1);
+
+    cy.get(".modal [name='locationClassKey']").select(2);
+
+    const newLocationName = "New Location-" + randomString();
+
+    cy.get(".modal [name='locationName']").clear().type(newLocationName);
+
+    cy.get(".modal form").submit();
+
+    cy.get(".modal").should("not.exist");
+
+    cy.get("[data-cy='results'] a")
+      .contains(newLocationName);
+  });
+
+  it("Removes a location", () => {
+
+    cy.get("[data-cy='results'] a")
+      .first()
+      .click({ force: true });
+
+    cy.get(".modal")
+      .should("be.visible")
+      .should("have.length", 1);
+
+    cy.get(".modal [name='locationName']").invoke("val").then((locationName: string) => {
+
+      cy.get(".modal [data-cy='remove']").click();
+
+      cy.focused().click();
+
+      cy.get(".modal").should("not.exist");
+
+      cy.get("[data-cy='results'] a")
+        .contains(locationName)
+        .should("have.length", 0);
+    });
   });
 });
