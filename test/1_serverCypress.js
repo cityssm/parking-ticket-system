06e0c59b394dd7db614a5d9bf85237484cee82b8
Dan Gowans <dangowans@users.noreply.github.com>
2023-10-23 13:18:45 -0400

    add firefox testing

--- test/1_serverCypress.js
+++ test/1_serverCypress.js
@@ -3,10 +3,28 @@ import { exec } from 'node:child_process';
 import * as http from 'node:http';
 import { app } from '../app.js';
 import { portNumber } from './_globals.js';
+const cypressTimeoutMillis = 30 * 60 * 60 * 1000;
+function runCypress(browser, done) {
+    let cypresssCommand = `cypress run --config-file cypress.config.ts --browser ${browser}`;
+    if ((process.env.CYPRESS_RECORD_KEY ?? '') !== '') {
+        cypresssCommand += ` --tag "${browser},${process.version}" --record`;
+    }
+    const childProcess = exec(cypresssCommand);
+    childProcess.stdout?.on('data', (data) => {
+        console.log(data);
+    });
+    childProcess.stderr?.on('data', (data) => {
+        console.error(data);
+    });
+    childProcess.on('exit', (code) => {
+        assert.ok(code === 0);
+        done();
+    });
+}
 describe('parking-ticket-system', () => {
     const httpServer = http.createServer(app);
     let serverStarted = false;
-    before(async () => {
+    before(() => {
         httpServer.listen(portNumber);
         httpServer.on('listening', () => {
             serverStarted = true;
@@ -23,22 +41,11 @@ describe('parking-ticket-system', () => {
         assert.ok(serverStarted);
     });
     describe('Cypress tests', () => {
-        it('should run Cypress tests', (done) => {
-            let cypresssCommand = 'cypress run --config-file cypress.config.ts --browser chrome';
-            if ((process.env.CYPRESS_RECORD_KEY ?? '') !== '') {
-                cypresssCommand += ' --record';
-            }
-            const childProcess = exec(cypresssCommand);
-            childProcess.stdout?.on('data', (data) => {
-                console.log(data);
-            });
-            childProcess.stderr?.on('data', (data) => {
-                console.error(data);
-            });
-            childProcess.on('exit', (code) => {
-                assert.ok(code === 0);
-                done();
-            });
-        }).timeout(30 * 60 * 60 * 1000);
+        it('should run Cypress tests in Chrome', (done) => {
+            runCypress('chrome', done);
+        }).timeout(cypressTimeoutMillis);
+        it('should run Cypress tests in Firefox', (done) => {
+            runCypress('firefox', done);
+        }).timeout(cypressTimeoutMillis);
     });
 });
--- test/1_serverCypress.ts
+++ test/1_serverCypress.ts
@@ -8,12 +8,37 @@ import { app } from '../app.js'
 
 import { portNumber } from './_globals.js'
 
+const cypressTimeoutMillis = 30 * 60 * 60 * 1000
+
+function runCypress(browser: 'chrome' | 'firefox', done: () => void): void {
+  let cypresssCommand = `cypress run --config-file cypress.config.ts --browser ${browser}`
+
+  if ((process.env.CYPRESS_RECORD_KEY ?? '') !== '') {
+    cypresssCommand += ` --tag "${browser},${process.version}" --record`
+  }
+
+  const childProcess = exec(cypresssCommand)
+
+  childProcess.stdout?.on('data', (data) => {
+    console.log(data)
+  })
+
+  childProcess.stderr?.on('data', (data) => {
+    console.error(data)
+  })
+
+  childProcess.on('exit', (code) => {
+    assert.ok(code === 0)
+    done()
+  })
+}
+
 describe('parking-ticket-system', () => {
   const httpServer = http.createServer(app)
 
   let serverStarted = false
 
-  before(async () => {
+  before(() => {
     httpServer.listen(portNumber)
 
     httpServer.on('listening', () => {
@@ -34,28 +59,12 @@ describe('parking-ticket-system', () => {
   })
 
   describe('Cypress tests', () => {
-    it('should run Cypress tests', (done) => {
-      let cypresssCommand =
-        'cypress run --config-file cypress.config.ts --browser chrome'
+    it('should run Cypress tests in Chrome', (done) => {
+      runCypress('chrome', done)
+    }).timeout(cypressTimeoutMillis)
 
-      if ((process.env.CYPRESS_RECORD_KEY ?? '') !== '') {
-        cypresssCommand += ' --record'
-      }
-
-      const childProcess = exec(cypresssCommand)
-
-      childProcess.stdout?.on('data', (data) => {
-        console.log(data)
-      })
-
-      childProcess.stderr?.on('data', (data) => {
-        console.error(data)
-      })
-
-      childProcess.on('exit', (code) => {
-        assert.ok(code === 0)
-        done()
-      })
-    }).timeout(30 * 60 * 60 * 1000)
+    it('should run Cypress tests in Firefox', (done) => {
+      runCypress('firefox', done)
+    }).timeout(cypressTimeoutMillis)
   })
 })
