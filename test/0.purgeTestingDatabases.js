1a3afd9260ce1f8a60eede06525e866b3b47539b
Dan Gowans <dangowans@users.noreply.github.com>
2023-11-07 10:39:08 -0500

    ensure databases properly reinitialized

--- test/0.purgeTestingDatabases.js
+++ test/0.purgeTestingDatabases.js
@@ -1,38 +1,24 @@
 import * as assert from 'node:assert';
-import { unlink } from 'node:fs';
+import { unlink } from 'node:fs/promises';
 import { parkingDB_testing, nhtsaDB_testing } from '../data/databasePaths.js';
 import { initNHTSADB } from '../database/nhtsaDB/initializeDatabase.js';
 import { initializeDatabase as initializeParkingDatabase } from '../database/parkingDB/initializeDatabase.js';
-describe('Reinitialize ' + parkingDB_testing, () => {
-    it('Purges ' + parkingDB_testing, (done) => {
-        unlink(parkingDB_testing, (error) => {
-            if (error) {
-                assert.fail();
-            }
-            else {
-                assert.ok(true);
-            }
-            done();
-        });
-    });
-    it('Creates ' + parkingDB_testing, () => {
+describe('Reinitialize databases', () => {
+    it(`Reinitialize ${parkingDB_testing}`, () => {
+        try {
+            void unlink(parkingDB_testing);
+        }
+        catch {
+        }
         const success = initializeParkingDatabase();
         assert.ok(success);
     });
-});
-describe('Reinitialize ' + nhtsaDB_testing, () => {
-    it('Purges ' + nhtsaDB_testing, (done) => {
-        unlink(nhtsaDB_testing, (error) => {
-            if (error) {
-                assert.fail();
-            }
-            else {
-                assert.ok(true);
-            }
-            done();
-        });
-    });
-    it('Creates ' + nhtsaDB_testing, () => {
+    it(`Reinitialize ${nhtsaDB_testing}`, () => {
+        try {
+            void unlink(nhtsaDB_testing);
+        }
+        catch {
+        }
         const success = initNHTSADB();
         assert.ok(success);
     });
